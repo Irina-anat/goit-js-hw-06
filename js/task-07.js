318b61a42f6e0db10a69ca96e44055fc949359be
Iryna Rozmaita <iriszechka@ukr.net>
2023-03-27 22:41:30 +0300

    доопрацювання

--- js/task-07.js
+++ js/task-07.js
@@ -6,10 +6,13 @@ console.dir(textValue)
 
 inputControl.addEventListener("input", onInput)
 
+textValue.style.fontSize = `${inputControl.value}px`;
+
 function onInput(event) {
-    const fontSize = event.currentTarget.valueAsNumber + "px";
-    console.log(event.currentTarget.valueAsNumber)
-    textValue.style.fontSize = fontSize;
+   const fontSize = `${event.currentTarget.valueAsNumber}px`;
+   console.log(event.currentTarget.valueAsNumber)
+   textValue.style.fontSize = fontSize;
+   console.log(textValue.style.fontSize)
 }
 
 
